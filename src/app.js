b28ec9da135e7ea189d6ba48b19e768ba2db1f31
raniyaptla <ra.niyapoonthala@gmail.com>
2023-11-06 13:08:31 +0530

    mes

--- src/app.js
+++ src/app.js
@@ -23,28 +23,57 @@ const PLAYERS = [
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
+    let detailedPlayers = [];
 
     // Instead of forloop use Map method
     // Code here
+    players.forEach((anil, index) => {
+        detailedPlayers.push({
+          name: anil,
+          strength: getRandomStrength(),
+          image: 'images/super-' + (index + 1) + '.png',
+          type: index % 2 == 0 ? 'hero' : 'villain',
+          id: index + 1,
+        });
+      });
+      return detailedPlayers;
 
-    return detailedPlayers;
+    
 }
 
 // getting random strength
 const getRandomStrength = () => {
     return Math.ceil(Math.random() * 100);
 }
-
+    const view = (playerObj) => {
+        let player = document.createElement('div');
+        player.classList.add('player');
+        let image = document.createElement('img');
+        image.setAttribute('src', playerObj.image);
+        image.setAttribute('alt', '');
+        let name = document.createElement('div');
+        name.className = 'name';
+        name.textContent = playerObj.name;
+        let strength = document.createElement('div');
+        strength.textContent = playerObj.strength;
+        strength.className = 'strength';
+        player.append(image, name, strength);
+        return player;
+    }
 // Build player template
 const buildPlayers = (players, type) => {
-    let fragment = '';
+    let fragment = document.createElement('div');
+  players
+    .filter((player) => player.type == type)
+    .forEach((player) => fragment.append(view(player)));
+  return fragment.innerHTML;
+    
 
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
+    
 
-    return fragment;
 }
 
 // Display players in HTML
